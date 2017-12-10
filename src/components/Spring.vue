9c395d3a50b32d5ab4cef02db2dfeb8f5798e9b9
Bastien <bastienrbt@protonmail.com>
2017-12-10 22:11:50 +0100

    Some new things for flower animation

--- src/components/Spring.vue
+++ src/components/Spring.vue
@@ -2,77 +2,69 @@
   <div>
     <Countdown date="March 20, 2018" until="Spring"></Countdown>
     <div id="animation">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 493.7 870.6" style="enable-background:new 0 0 493.7 870.6;" xml:space="preserve" id="flower">
-      	<g id="leaf">
-      		<path class="leaf__main" d="M248.3,432.2c28.1-43.2,66.9-52,88.7-53.3c15.4-0.9,28,12.8,24.8,28c-0.6,2.8-1.5,5.7-2.9,8.8
-      		c-10.6,23.1-19.8,47.5-109.5,63.3"/>
-      		<path class="leaf__shadow" d="M351.5,386.5c0.9,3,1.5,5.8,0.8,9.1c-0.5,2.4-1.4,5.1-2.7,7.8c-9.7,20.5-39.1,47.5-100.7,72.3
-      		c88.1-15.1,97.2-38.4,107.6-60.4c1.4-3,2.3-5.8,2.9-8.4C361,398.8,357.5,391.4,351.5,386.5z"/>
-      	</g>
-      	<g id="stem">
-      		<!-- <polyline points="247.4,512.2 249.4,279 	"/> -->
-      		<line x1="247.4" x2="249.4" y1="512.2" y2="279" />
-      	</g>
-      	<g id="pot">
-      		<g id="pot-bottom">
-      			<path class="pot__main" d="M411,576.2l-337.6,0.4c-6.7,0-11.7,6.3-10.6,13.1L105,836.1c0.9,5.3,5.4,9.2,10.6,9.2h248
-      			c5.1,0,9.5-3.8,10.6-9l47.4-246.8C422.8,582.7,417.7,576.2,411,576.2z"/>
-      			<path class="pot__left" d="M411.2,579.4l-65.8,0.1c6.1,0.7,10.5,6.6,9.3,12.9l-47.4,241.3c-1,5.1-5.4,8.8-10.6,8.8h67.1
-      			c5.1,0,9.5-3.7,10.6-8.8l47.4-241.3C423.1,585.7,418,579.4,411.2,579.4z"/>
-      			<path class="pot__right" d="M76.3,579.4c-6.7,0-11.7,6.1-10.6,12.8l42.2,240.9c0.9,5.2,5.4,9,10.6,9h82.3c-5.2,0-9.7-3.8-10.6-9
-      			L148,592.2c-1.2-6.7,3.9-12.8,10.6-12.8"/>
-      		</g>
-      		<g id="pot-top">
-      			<path class="pot__main" d="M443.5,511H40.9c-11.8,0-21.4,9.6-21.4,21.4v41.7c0,11.8,9.6,21.4,21.4,21.4h402.6c11.8,0,21.4-9.6,21.4-21.4
-      			v-41.7C464.8,520.6,455.3,511,443.5,511z"/>
-      			<path class="pot__right" d="M66.6,572.8v-38.9c0-11,9.6-19.9,21.4-19.9H42.2c-11.8,0-21.4,8.9-21.4,19.9v38.9c0,11,9.6,19.9,21.4,19.9H88
-      			C76.2,592.8,66.6,583.8,66.6,572.8z"/>
-      			<path class="pot__left" d="M417.9,573v-39.2c0-11.1-9.6-20.1-21.4-20.1h45.8c11.8,0,21.4,9,21.4,20.1V573c0,11.1-9.6,20.1-21.4,20.1
-      			h-45.8C408.3,593.1,417.9,584.1,417.9,573z"/>
-      		</g>
-      	</g>
-      	<g id="petals">
-      		<path d="M294.2,115.7c0,0,6.6-82.3,70.8-59.8c40.7,14.3,45.8,57.2,31.2,74.9c-9.4,11.4-29.4,21.7-61.3,40.3l0,0
-      		c0,0,52.4-47.7,82.2,13.5c18.8,38.8-7.8,72.8-30.6,75.2c-14.7,1.5-29-0.3-62.4-16.4l0,0c0,0,68.8,15.6,40.7,77.5
-      		c-17.8,39.3-61,40.6-77.3,24.5c-10.6-10.3-30.4-30-39.3-66l-0.4,1.2c0,0-21.2,84.6-85.4,62.1c-40.7-14.3-45.8-57.2-31.2-74.9
-      		c9.4-11.4,20.5-19.6,55.6-31.7l0.4-0.2c0,0-93.4,45.1-117.7-6.1c-18.5-38.9,7.8-72.8,30.6-75.2c14.7-1.5,29.8,1.3,63.2,17.4l0.6,1
-      		c0,0-78.5-28.7-60.8-80.9c13.9-40.8,57.2-45.8,74.9-31.2C189.3,70.3,198,83,210,118.1l0.2-0.5c0,0-39.2-68.7,16.4-96
-      		c38.7-19,67.7,6.8,71.6,29.4c2.5,14.4,4.4,24.4-3.7,61.7"/>
-      	</g>
-      	<g id="pistil">
-      		<ellipse transform="matrix(1.564399e-02 -0.9999 0.9999 1.564399e-02 59.7252 442.1572)" cx="254.4" cy="190.7" rx="66.4" ry="66.4"/>
-      	</g>
+      <svg version="1.1" id="main" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      	 viewBox="0 0 301.6 553.6" style="enable-background:new 0 0 301.6 553.6;" xml:space="preserve">
+        <g id="leaf">
+          <path class="main" d="M154.6,278c18.2-28.1,43.4-33.8,57.6-34.6c10-0.6,18.2,8.3,16.1,18.2c-0.4,1.8-1,3.7-1.9,5.7
+            c-6.9,15-12.9,30.9-71.1,41.1"/>
+          <path class="shadow" d="M222.9,248c0.6,2,0.9,3.8,0.5,6c-0.3,1.6-0.9,3.3-1.7,5.2c-6.2,13.5-24.9,31.4-64.3,47.7
+            c56.3-10,62.1-25.3,68.7-39.8c0.9-2,1.5-3.8,1.8-5.5C229,256.1,226.8,251.2,222.9,248z"/>
+        </g>
+        <g id="stick">
+          <line x1="154.1" y1="329.9" x2="155.3" />
+        </g>
+        <g id="pot">
+          <g id="pot-bottom">
+            <path class="main" d="M260.3,373.4L41,373.6c-4.3,0-7.6,4.1-6.9,8.5l27.4,160.1c0.6,3.5,3.5,6,6.9,6h161.1c3.3,0,6.2-2.4,6.9-5.8
+              L267.2,382C268.1,377.5,264.7,373.4,260.3,373.4z"/>
+            <path class="right" d="M259.5,375.6l-42.3,0.1c3.9,0.5,6.8,4.3,6,8.4l-30.5,156.3c-0.6,3.3-3.5,5.7-6.8,5.7H229
+              c3.3,0,6.1-2.4,6.8-5.7L266.3,384C267.2,379.7,263.9,375.6,259.5,375.6z"/>
+            <path class="left" d="M43.9,377.3c-4.3,0-7.5,3.9-6.8,8.2l26.5,154.3c0.6,3.3,3.4,5.8,6.7,5.8l52.4,0.1c-3.3,0-6.2-2.4-6.7-5.8
+              L89.6,385.6c-0.7-4.3,2.5-8.2,6.8-8.2"/>
+          </g>
+          <g id="pot-top">
+            <path class="main" d="M277.8,329.2H23.5C13.8,329.2,6,337,6,346.7v19.9c0,9.7,7.8,17.5,17.5,17.5h254.3c9.7,0,17.5-7.8,17.5-17.5
+              v-19.9C295.3,337,287.5,329.2,277.8,329.2z"/>
+            <path class="left" d="M38.7,369.2v-25.4c0-7.2,6.4-13,14.3-13H22.4c-7.9,0-14.3,5.8-14.3,13v25.4c0,7.2,6.4,13,14.3,13H53
+              C45.1,382.1,38.7,376.3,38.7,369.2z"/>
+            <path class="right" d="M261.1,369.5l0-25.6c0-7.2-6.8-13.1-15.1-13.1l32.5,0c8.4,0,15.1,5.9,15.1,13.1l0,25.6
+              c0,7.2-6.8,13.1-15.2,13.1l-5,0l-27.5,0C254.3,382.6,261.1,376.7,261.1,369.5z"/>
+          </g>
+        </g>
+        <g id="petals">
+          <path d="M184.5,72.3c0,0,4.3-53.5,46-38.8c26.4,9.3,29.7,37.1,20.3,48.7c-6.1,7.4-19.1,14.1-39.9,26.2l0,0
+            c0,0,34.1-31,53.4,8.8c12.2,25.2-5,47.3-19.8,48.8c-9.6,1-18.9-0.2-40.6-10.7l0,0c0,0,44.7,10.1,26.4,50.4
+            c-11.6,25.5-39.6,26.4-50.2,15.9c-6.9-6.7-19.8-19.5-25.6-42.9l-0.3,0.8c0,0-13.8,55-55.5,40.3c-26.4-9.3-29.7-37.1-20.3-48.7
+            c6.1-7.4,13.3-12.8,36.1-20.6l0.3-0.1c0,0-60.7,29.3-76.5-3.9c-12-25.3,5-47.3,19.8-48.8c9.6-1,19.4,0.8,41.1,11.3l0.4,0.6
+            c0,0-51-18.7-39.5-52.6c9-26.5,37.1-29.7,48.7-20.3c7.4,6.1,13,14.4,20.9,37.1l0.1-0.3c0,0-25.5-44.6,10.7-62.3
+            c25.1-12.3,44,4.4,46.5,19.1c1.6,9.3,2.8,15.8-2.4,40.1"/>
+        </g>
+        <g id="pistil">
+          <ellipse transform="matrix(1.564399e-02 -0.9999 0.9999 1.564399e-02 35.0692 277.8048)" cx="158.6" cy="121.1" rx="43.2" ry="43.2"/>
+        </g>
       </svg>
-      <svg version="1.1" id="wandering_can" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-      	 viewBox="0 0 684 580" style="enable-background:new 0 0 684 580;" xml:space="preserve">
-        <path class="wandering_can__content" d="M327.2,169.2c-17.6,12-64.3,48.7-86.2,115.9c-0.4,1.4-0.7,2.8-0.7,4.2c-0.1,5.3,0,19,4.3,31.9
-        	c5.5,16.4,27.3,76.5,32.9,90.1c5,12,54.6,127.6,65.8,153.6c1.3,3.1,4.6,5,8,4.6c28.5-3.2,158.3-23,268.3-127.1
-        	c2.3-2.2,3.1-5.6,1.9-8.6l-87.2-216.6c0,0-11-24.3-30.6-42.3c-2.9-2.6-7.3-2.7-10.3-0.2c-11.3,9.7-44.1,33.7-112.3,56.8
-        	c-3.5,1.2-7.3-0.3-9.2-3.4l-33.4-56.4C335.9,167.8,330.9,166.7,327.2,169.2z"/>
-        <path class="wandering_can__content_center" d="M610.3,406.6C437.7,514.8,326,525.8,326,525.8L250.4,338c0,0,169.1-13.9,282-122.9L610.3,406.6z"/>
-        <path class="wandering_can__content_shadow" d="M607.8,405.3l-76.1-186.5c-13.1,12.6-26.9,23.9-41.2,34C490.6,252.8,591.9,413.7,607.8,405.3z"/>
-        <path class="wandering_can__handle" d="M268.6,223.7c0,0-42.2-61.1,0.5-123.9c62.6-92,301.9-152.6,379.6,75.4c13.7,40.2-4.5,105-70.5,146.8l8,20.5
-        	c72.1-45.7,98.6-120.1,83.7-164C585-70.6,323.5-4.4,255.1,96.1c-46.7,68.6,6.7,143.2,6.7,143.2L268.6,223.7z"/>
-        <path class="wandering_can__pipe" d="M278.7,409.1L319.9,510L95.1,442c-1.4-0.7-3-3-2.8-5.4l1.2-24.8c0.1-2.9,2.6-5.2,5.5-5.2L278.7,409.1z"/>
-        <polygon class="wandering_can__bar" points="186,406.7 187.7,405.3 255.1,357.4 264.4,379.5 227,406.7 "/>
-        <path class="wandering_can__head" d="M88.9,442.3l-70.2,23.8c-3.9,1.3-7.9-1.8-7.5-5.9l7.1-84.5c0.3-4.1,4.8-6.5,8.4-4.6L92,405.7
-        	c2,1.1,3.2,3.3,3,5.5l-2.2,26.1C92.6,439.6,91.1,441.5,88.9,442.3z"/>
-        <path class="wandering_can__pipe_shadow" d="M304.5,479.5L96.7,416.3l-0.9,18.6c-0.1,2.4,1.4,4.6,2.7,5.3l216.7,65.9L304.5,479.5z"/>
-        <path class="wandering_can__bottom_shadow" d="M610.5,413.6c-6.2,8.5-93.5,121.1-259.6,152.8c212.9-40.6,271-131.2,271-131.2L610.5,413.6z"/>
+      <svg version="1.1" id="watering-can" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      	 viewBox="0 0 345.3 248.6" style="enable-background:new 0 0 345.3 248.6;" xml:space="preserve">
+        <path class="st0" d="M194.6,60.6c-9.7,1.9-36.5,9.2-58.1,34c-0.4,0.5-0.8,1.1-1.1,1.7c-1,2.3-3.5,8.2-4,14.5
+          c-0.6,8-2.2,37.8-2.3,44.6c-0.1,6.1,0.2,64.7,0.3,77.9c0,1.6,1,3,2.6,3.4c12.8,3.8,72.1,19,138.3-5.7c1.4-0.5,2.3-1.9,2.4-3.4
+          l2-108.8c0,0-0.3-12.4-5.4-23.7c-0.8-1.7-2.6-2.5-4.4-1.9c-6.6,2.1-25,6.5-58.5,3.9c-1.7-0.1-3.1-1.4-3.3-3.1l-4.1-30.3
+          C198.6,61.6,196.7,60.2,194.6,60.6z"/>
+        <path class="st0" d="M272.2,214.1c-93.4,15-143.1-0.6-143.1-0.6l1.8-94.2c0,0,74.7,24.8,142.8-1.4L272.2,214.1z"/>
+        <path class="st1" d="M271.9,213.9l1.5-94.8c-8,3.1-16,5.4-24,7.2C249.3,126.3,263.5,214.6,271.9,213.9z"/>
+        <path class="st2" d="M159.5,73.3c0,0-7-33.9,22.8-53.1C226-7.8,339.7,9.8,331.5,121.8c-1.4,19.7-21,44.2-57,50.1l-0.3,10.3
+          c39.3-6.5,64.2-33.6,65.8-55.1C348.9,4.7,224.6-14.5,177,16.1c-32.5,20.9-23.2,62.7-23.2,62.7L159.5,73.3z"/>
+        <path class="st0" d="M130.1,154.7l-0.7,50.8l-84-70.1c-0.5-0.6-0.7-1.8-0.2-2.8l5-10.4c0.6-1.2,2-1.8,3.3-1.2L130.1,154.7z"/>
+        <polygon class="st3" points="90.7,136.8 91.7,136.5 129.4,128.2 129.4,139.4 108.3,144.3 "/>
+        <path class="st3" d="M42.6,134.4l-34.5-2.6c-1.9-0.1-3.1-2.2-2.2-3.9l18.4-35c0.9-1.7,3.2-1.9,4.4-0.4l21.7,26.7
+          c0.7,0.8,0.8,2,0.3,2.9L45.2,133C44.7,133.9,43.7,134.5,42.6,134.4z"/>
+        <path class="st1" d="M128.1,189.5l-77.2-64.6l-3.7,7.8c-0.5,1-0.2,2.2,0.2,2.7l80.5,67.4L128.1,189.5z"/>
       </svg>
-      <svg version="1.1" id="drop" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 76 98" style="enable-background:new 0 0 76 98;" xml:space="preserve">
-      	<g>
-      		<g transform="translate(-162.4 -231.41)">
-      			<g id="drop__center" transform="translate(2262.9 200)">
-      				<path d="M-2031.6,97.2c-3,16.3-18,27.2-33.6,24.3c-15.6-2.8-26.1-18.4-22.9-34.6
-      				c4.4-21.7,21.5-27.8,38.4-50.6C-2041,62.9-2028.6,80.9-2031.6,97.2L-2031.6,97.2z"/>
-      			</g>
-      		</g>
-      		<g id="drop__shadow">
-      			<path d="M50.9,4.8C57.2,31.1,62,46.8,59,63c-3,16.3-13.4,28.8-26.2,26.5c0.8,0.2,1.6,0.4,2.5,0.6
-      			C50.9,92.9,66,82,68.9,65.8C71.9,49.5,59.5,31.5,50.9,4.8z"/>
-      		</g>
-      	</g>
+      <svg version="1.1" id="droplet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      	 viewBox="0 0 28.1 41.6" style="enable-background:new 0 0 28.1 41.6;" xml:space="preserve">
+        <path id="path2530_1_" class="st0" d="M27.4,27c0,7.7-6,14-13.4,14S0.5,34.7,0.6,27C0.8,16.7,8.1,12.5,14,0.6
+        	C20.3,12.1,27.4,19.3,27.4,27L27.4,27z"/>
+        <path class="st1" d="M14.1,0.6c5.1,11.5,8.6,18.3,8.6,26S19,40.9,12.9,40.9c0.4,0,0.8,0.1,1.2,0.1c7.4,0,13.4-6.3,13.4-14
+        	C27.4,19.3,20.3,12.1,14.1,0.6z"/>
       </svg>
     </div>
   </div>
@@ -84,37 +76,89 @@ import Countdown from '../components/partials/Countdown.vue';
 export default {
   components: { Countdown },
   mounted: function () {
-    this.stem()
+    this.stick()
+    this.petals()
+    this.pistil()
     this.leaf()
-    this.wandering_can()
+    this.wateringCan()
+    this.droplet()
+    this.pot()
   },
   methods: {
-    stem: function () {
+    stick: function () {
       anime({
-        targets: '#stem line',
-        y2: [515.2, 279],
+        targets: '#main #stick line',
+        y2: [329.9, 145],
+        duration: 1600,
+        easing: 'easeInOutQuart'
+      })
+    },
+    petals: function () {
+      anime({
+        targets: '#main #petals',
+        scale: [0, 1],
         duration: 2000,
-        easing: 'linear',
-        loop: true,
-        direction: 'alternate'
+        delay: 1150,
+        easing: 'easeInOutQuart'
+      })
+    },
+    pistil: function () {
+      anime({
+        targets: '#main #pistil',
+        scale: [0, 1],
+        duration: 2000,
+        delay: 250,
+        easing: 'easeInOutQuart'
       })
     },
     leaf: function () {
       anime({
-        targets: '#leaf',
-        scale: [0, 1],
-        duration: 3000,
-        loop: true,
-        direction: 'alternate'
+        targets: '#main #leaf',
+        rotateY: ['-90deg', 0],
+        duration: 2000,
+        delay: 2800
       })
     },
-    wandering_can: function () {
-      anime ({
-        targets: '#wandering_can',
-        rotate: [20, -25],
+    wateringCan: function () {
+      anime({
+        targets: '#watering-can',
+        rotate: [3, -30],
+        direction: 'alternate',
+        duration: 1250,
+        easing: 'easeInOutQuart',
+        delay: 400
+      })
+      anime({
+        targets: '#watering-can',
         duration: 1500,
-        loop: true,
-        easing: 'linear',
+        opacity: 0,
+        easing: 'easeInExpo',
+        delay: 3200
+      })
+    },
+    pot: function () {
+      anime({
+        targets: '#main',
+        bottom: ['5vh', '15vh'],
+        delay: 4200,
+        duration: 1800
+      })
+    },
+    droplet: function () {
+      anime({
+        targets: '#droplet',
+        rotate: [0, 20],
+        left: ['56.5%', '53%'],
+        bottom: ['49.5vh', '15vh'],
+        duration: 1200,
+        delay: 700,
+        easing: 'easeInExpo'
+      })
+      anime({
+        targets: '#droplet',
+        opacity: [0, 1],
+        delay: 1400,
+        duration: 1200,
         direction: 'alternate'
       })
     }
@@ -124,104 +168,96 @@ export default {
 
 <style lang='stylus' scoped>
 #animation
-  #flower
-    position absolute
-    height 32vh
-    bottom 5vh
+  #main
+    height 50%
     #leaf
-      transform-origin 50% 50%
-      .leaf__main
+      transform-origin 0% 50%
+      .main
         fill #B5EAC2
-        stroke #020202
-        stroke-width 6
-        stroke-linecap round
-        stroke-linejoin round
+        stroke #B66A5C
+        stroke-width 2
         stroke-miterlimit 10
-      .leaf__shadow
-        fill #8DC499
+      .shadow
+        fill #9ECCA9
 
-    #stem
+    #stick
       line
-        stroke #000000
-        stroke-width 8
-        stroke-linecap round
+        fill #DB9C8F
+        stroke #B66A5C
+        stroke-width 3
         stroke-miterlimit 10
 
-    #pot-top .pot__main,
-    #pot-bottom .pot__main
-      fill #F3DBB8
-      stroke #000000
-      stroke-width 5
-      stroke-linecap round
-      stroke-linejoin round
-      stroke-miterlimit 10
-
-    #pot-top .pot__left,
-    #pot-bottom .pot__left
-      fill #C9B497
-
-    #pot-top .pot__right,
-    #pot-bottom .pot__right
-      fill #FCEED3
+    #pot
+      .main
+        fill #EDCDC7
+        stroke #B66A5C
+        stroke-width 2
+        stroke-miterlimit 10
+      .right
+        fill #DB9C8F
+      .left
+        fill #FFE9E5
 
     #petals
-      fill #FCAA9F
-      stroke #020202
-      stroke-width 7
-      stroke-linecap round
-      stroke-linejoin round
-      stroke-miterlimit 10
+      transform-origin 50% 50%
+      path
+        fill #FFD2CA
+        stroke #B66A5C
+        stroke-width 2
+        stroke-miterlimit 10
 
     #pistil
+      transform-origin 50% 50%
       ellipse
-        fill #FDD007
-        stroke #000000
-        stroke-width 6
+        fill #FFC775
+        stroke #B66A5C
+        stroke-width 2
         stroke-miterlimit 10
-  #wandering_can
+  #watering-can
     position absolute
-    height 28vh
-    bottom 38vh
-    right 15%
-    .wandering_can__content, .wandering_can__bar
+    width 18%
+    bottom 45vh
+    left 55%
+    transform-origin 50% 50%
+    +below(750px)
+      display none
+    .st0
       fill #C3E0FF
-      stroke #000000
-      stroke-width 5
-      stroke-linecap round
-      stroke-linejoin round
+      stroke #3B5C7F
+      stroke-width 2
       stroke-miterlimit 10
-
-    .wandering_can__content_center, .wandering_can__pipe, .wandering_can__head
+    .st1
       fill #E7F3FF
-      stroke #000000
-      stroke-width 5
-      stroke-linecap round
-      stroke-linejoin round
-      stroke-miterlimit 10
-
-    .wandering_can__pipe_shadow, .wandering_can__bottom_shadow, .wandering_can__content_shadow
-      fill #D9E2F2
-
-    .wandering_can__handle
+    .st2
       fill #E7F3FF
-      stroke #000000
-      stroke-width 3
+      stroke #3B5C7F
+      stroke-width 2
       stroke-linecap round
       stroke-linejoin round
       stroke-miterlimit 10
-  #drop
-    display none
-    height 5vh
-    #drop, #drop__center path
+    .st3
+      fill #C3E0FF
+      stroke #3B5C7F
+      stroke-width 2
+      stroke-linecap round
+      stroke-linejoin round
+      stroke-miterlimit 10
+  #droplet
+    position absolute
+    width 1vw
+    left 56.5%
+    bottom 49.5vh
+    z-index -3
+    transform-origin 50% 50%
+    .st0
       opacity 0.6985
-      fill #0041F1
+      fill #76B8FF
+      fill-opacity 0.526
+      enable-background new
+    .st1
+      opacity 0.6985
+      fill #9CB3CC
       fill-opacity 0.526
       enable-background new
 
-    #drop__shadow
-      path
-          opacity 0.6985
-          fill #0041F1
-          fill-opacity 0.526
-          enable-background new
 </style>
